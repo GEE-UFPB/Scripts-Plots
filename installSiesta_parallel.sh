e5d57f159830f2da72f4f612faa4adfdb3246d1b
GEE-UFPB <66834837+GEE-UFPB@users.noreply.github.com>
2020-08-05 14:39:39 -0300

    Add files via upload

---
+++ installSiesta_parallel.sh
@@ -0,0 +1,40 @@
+#Instalador de programas
+########################
+#Atualizar o sistema
+echo "Atualizar o sistema"
+sudo apt-get update
+########################
+echo "Instalar pacotes de compiladores - visualizadores e bibliotecas"
+sudo apt-get install gfortran -y
+sudo apt-get install vim -y
+sudo apt-get install build-essential checkinstall -y
+sudo apt-get install openmpi-bin openmpi-doc libopenmpi-dev -y
+sudo apt-get install libblacs-mpi-dev -y
+sudo apt-get install liblapack-dev -y
+sudo apt-get install liblapack3 -y
+sudo apt-get install libopenblas-base -y
+sudo apt-get install libopenblas-dev -y
+sudo apt-get install liblapack-dev libopenblas-dev -y
+#########################
+#Entrar na pasta de instalação
+cd /usr/local/lib/
+sudo chmod +x .
+# Instalar o Siesta
+#Baixar a versão desejada
+sudo wget https://launchpad.net/siesta/4.0/4.0.1/+download/siesta-4.0.1.tar.gz
+sudo tar -xzvf siesta-4.0.1.tar.gz
+cd siesta-4.0.1/
+cd Obj/
+sh ../Src/obj_setup.sh
+cd ../Src
+./configure --enable-mpi FC=/usr/bin/mpif90 --with-blacs=/usr/lib/libblacs-openmpi.so --with-scalapack=/usr/lib/libscalapack-openmpi.so
+sudo rm arch.make
+sudo wget https://github.com/GEE-UFPB/Scripts-Plots/raw/master/arch.make
+cp arch.make ../Obj
+cd ../Obj
+make
+#########################
+# Copiando para um local mais adequado
+sudo cp /usr/local/lib/siesta-4.0.1/Obj/siesta /usr/local/bin
+cd 
+siesta
